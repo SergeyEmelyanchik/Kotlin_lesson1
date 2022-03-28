cb7affa47b98d476ecb1462853ca5a7bd7400d7d
Sergey <emelyanchik1980@gmail.com>
2022-03-28 13:53:11 +0300

    Домашнее задание к уроку №1 Добавил кнопку в разметку и повесил на неё clickListener в Activity

--- app/src/main/java/ru/geekbrains/kotlin_lesson1/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlin_lesson1/MainActivity.kt
@@ -1,14 +1,39 @@
-package ru.geekbrains.kotlin_lesson1;
+package ru.geekbrains.kotlin_lesson1
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.TextView
+import ru.geekbrains.kotlin_lesson1.repo.MyDataClass
+import ru.geekbrains.kotlin_lesson1.repo.MyTestObject
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+
     }
+
+    fun init() {
+        val myData = MyDataClass(1, "Минск")
+        val mySecondData = myData.copy()
+        mySecondData.town = "Минск"
+        mySecondData.weather = 0
+
+        val textView: TextView = findViewById(R.id.textView)
+        val button: Button = findViewById(R.id.btn1)
+        button.setOnClickListener {
+            textView.text = "Город: " + mySecondData.town + "\nТемпература: " + mySecondData.weather + " градусов!"
+            textView.append("\n" + MyTestObject.getMyTest())
+            MyTestObject.getTestForEach()
+        }
+
+
+    }
+
+
 }
---
+++ app/src/main/java/ru/geekbrains/kotlin_lesson1/repo/MyDataClass.kt
@@ -0,0 +1,19 @@
+package ru.geekbrains.kotlin_lesson1.repo
+
+import android.util.Log
+
+data class MyDataClass(var weather: Int, var town: String)
+
+
+object MyTestObject {
+
+    fun getMyTest() = "Привет"
+
+    fun getTestForEach() {
+        val list = listOf(1, 2, 3, 4)
+        list.forEach {
+            Log.d("*****", "$it Привет")
+        }
+    }
+
+}

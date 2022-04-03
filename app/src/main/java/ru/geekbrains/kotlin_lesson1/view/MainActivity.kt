b7741d6badb9bbe75253d03223c29f759ef8a664
Sergey <emelyanchik1980@gmail.com>
2022-04-03 22:54:38 +0300

    Домашнее задание к уроку №2 подготовил скелет приложения (MVVM) + viewBinding

---
+++ app/src/main/java/ru/geekbrains/kotlin_lesson1/view/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.geekbrains.kotlin_lesson1.view
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import ru.geekbrains.kotlin_lesson1.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment.newInstance()).commit()
+        }
+    }
+}
---
+++ app/src/main/java/ru/geekbrains/kotlin_lesson1/view/MainFragment.kt
@@ -0,0 +1,55 @@
+package ru.geekbrains.kotlin_lesson1.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import ru.geekbrains.kotlin_lesson1.R
+import ru.geekbrains.kotlin_lesson1.databinding.FragmentMainBinding
+import ru.geekbrains.kotlin_lesson1.viewmodel.MainViewModel
+
+
+class MainFragment : Fragment() {
+
+
+    lateinit var binding: FragmentMainBinding// утечка памяти
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnOne.setOnClickListener { }
+        // view.findViewById<Button>(R.id.btnOne).setOnClickListener {  }то же самое, что и выше но не ловит nuul point reception
+        // view.findViewById<TextView>(R.id.btnOne).setOnClickListener {  }
+
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        //val observer = Observer<Any>{ renderData(it) } пока обойдемся без лямды
+        val observer = object : Observer<Any> {
+            override fun onChanged(data: Any) {
+                renderData(data)
+            }
+        }
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+        viewModel.getWeather()
+    }
+
+    private fun renderData(data: Any) {
+        Toast.makeText(requireContext(), "ВСЁ РАБОТАЕТ", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}

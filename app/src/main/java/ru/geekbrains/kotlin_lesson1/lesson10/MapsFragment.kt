60892de7c4d188315f921a625eafdda5a4c9befc
Sergey <emelyanchik1980@gmail.com>
2022-06-01 21:13:21 +0300

    Домашнее задание к уроку №11 Добавил push notification

--- app/src/main/java/ru/geekbrains/kotlin_lesson1/lesson10/MapsFragment.kt
+++ app/src/main/java/ru/geekbrains/kotlin_lesson1/lesson10/MapsFragment.kt
@@ -3,6 +3,7 @@ package ru.geekbrains.kotlin_lesson1.lesson10
 import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,6 +17,8 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import ru.geekbrains.kotlin_lesson1.R
 import ru.geekbrains.kotlin_lesson1.databinding.FragmentMapsMainBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MapsFragment : Fragment() {
 
@@ -99,5 +102,34 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+        initView()
+    }
+
+    private fun initView() {
+        binding.buttonSearch.setOnClickListener {
+            val searchText = binding.searchAddress.text.toString()
+            searchText.isBlank()
+            searchText.isEmpty()
+            val geocoder = Geocoder(requireContext(), Locale.getDefault())
+            val results = geocoder.getFromLocationName(searchText, 1)
+            //TODO HW проверка results
+            val location = LatLng(
+                results[0].latitude,
+                results[0].longitude
+            )
+            map.addMarker(
+                MarkerOptions().position(
+                    location
+                )
+                    .title(searchText)
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_map_marker))
+
+            )
+            map.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    location, 10f
+                )
+            )
+        }
     }
 }
--- app/src/main/java/ru/geekbrains/kotlin_lesson1/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlin_lesson1/view/MainActivity.kt
@@ -1,12 +1,16 @@
 package ru.geekbrains.kotlin_lesson1.view
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import ru.geekbrains.kotlin_lesson1.MyApp
 import ru.geekbrains.kotlin_lesson1.R
 import ru.geekbrains.kotlin_lesson1.lesson10.MapsFragment
@@ -24,6 +28,57 @@ import kotlin.system.exitProcess
 
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val NOTIFICATION_ID_LOW = 1
+        private const val NOTIFICATION_ID_HIGH = 2
+        private const val CHANNEL_ID_LOW = "channel_id_1"
+        private const val CHANNEL_ID_HIGH = "channel_id_2"
+    }
+
+    private fun push() {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationBuilderLow = NotificationCompat.Builder(this, CHANNEL_ID_LOW).apply {
+            setSmallIcon(R.drawable.ic_map_pin)
+            setContentTitle(getString(R.string.notification_heading))
+            setContentText(getString(R.string.notification_mesage))
+            priority = NotificationManager.IMPORTANCE_HIGH
+        }
+
+        val notificationBuilderHigh = NotificationCompat.Builder(this, CHANNEL_ID_HIGH).apply {
+            setSmallIcon(R.drawable.ic_map_marker)
+            setContentTitle(getString(R.string.notification_heading_two))
+            setContentText(getString(R.string.notification_mesage_two))
+            priority = NotificationManager.IMPORTANCE_HIGH
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelNameLow = "Name $CHANNEL_ID_LOW"
+            val channelDescriptionLow = "Name $CHANNEL_ID_LOW"
+            val channelPriorityLow = NotificationManager.IMPORTANCE_LOW
+            val channelLow =
+                NotificationChannel(CHANNEL_ID_LOW, channelNameLow, channelPriorityLow).apply {
+                    description = channelDescriptionLow
+                }
+            notificationManager.createNotificationChannel(channelLow)
+        }
+        notificationManager.notify(NOTIFICATION_ID_LOW, notificationBuilderLow.build())
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelNameHigh = "Name $CHANNEL_ID_HIGH"
+            val channelDescriptionHigh = "Name $CHANNEL_ID_HIGH"
+            val channelPriorityHigh = NotificationManager.IMPORTANCE_HIGH
+            val channelHigh =
+                NotificationChannel(CHANNEL_ID_HIGH, channelNameHigh, channelPriorityHigh).apply {
+                    description = channelDescriptionHigh
+                }
+            notificationManager.createNotificationChannel(channelHigh)
+        }
+        notificationManager.notify(NOTIFICATION_ID_HIGH, notificationBuilderHigh.build())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -52,7 +107,7 @@ class MainActivity : AppCompatActivity() {
 /*        LocalBroadcastManager.getInstance(this)
             .registerReceiver(receiver, IntentFilter(BROADCAST_RECEIVER_CHANNEL_KEY))*/
         Thread { MyApp.getHistoryDAO().getAll() }.start()
-
+        push()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -72,12 +127,14 @@ class MainActivity : AppCompatActivity() {
             (R.id.actionThreads) -> {
                 supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.container, ThreadsFragment
-                    .newInstance())
+                    .replace(
+                        R.id.container, ThreadsFragment
+                            .newInstance()
+                    )
                     .addToBackStack("")
                     .commit()
             }
-            (R.id.action_menu_google_maps)->{
+            (R.id.action_menu_google_maps) -> {
                 supportFragmentManager
                     .beginTransaction()
                     .add(R.id.container, MapsFragment())
@@ -87,11 +144,12 @@ class MainActivity : AppCompatActivity() {
             R.id.content_provider ->
                 supportFragmentManager
                     .beginTransaction()
-                    .add(R.id.container, ContentProviderFragment
-                    .newInstance())
+                    .add(
+                        R.id.container, ContentProviderFragment
+                            .newInstance()
+                    )
                     .addToBackStack("")
                     .commit()
-
 
 
             (R.id.actionExit) -> {
